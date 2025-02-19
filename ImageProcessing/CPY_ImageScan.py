f85ed304025ba03db18a4a0b6948d693fe1778bb
Alan Weng <115497296+akkilin@users.noreply.github.com>
2025-02-18 21:26:10 -0500

    Function Planning

---
+++ ImageProcessing/CPY_ImageScan.py
@@ -0,0 +1,27 @@
+import numpy
+import matplotlib
+import cv2
+
+# Purpose: Scan media and digitalizes it                    FIXME
+#      
+# Constraints:
+#   Inputs: Image file in PNG, JPG, ...                     FIXME
+#   Outputs: Scanned Image file in JPG (or PDF)             FIXME
+
+
+
+# Purpose: Given a file in picture format, returns back black and white (?)   FIXME
+#      
+# Constraints:
+#   Inputs: File in picture format                          FIXME
+#   Outputs: Black and white version                        FIXME
+
+
+
+# Purpose: Given a pdf, returns back black and white (?)    FIXME
+#      
+# Constraints:
+#   Inputs: PDF                                             FIXME
+#   Outputs: Black and white version                        FIXME
+
+
