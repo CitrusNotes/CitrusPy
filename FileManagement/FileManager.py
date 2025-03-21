5b30ca070ca4c53959ac9eb07a6b3f1c07840de2
Hin Yan Liew <hinyan17@gmail.com>
2025-03-21 16:21:52 -0400

    file management function planning

---
+++ FileManagement/FileManager.py
@@ -0,0 +1,49 @@
+
+'''
+Object oriented design for the file management:
+File implements item
+Folder implements item
+Manager class
+'''
+
+
+# Purpose: Receive a request from frontend to view user's data
+# 
+# Constraints:
+# - Inputs: Frontend request (most likely in json format)
+# - Effects: Decides what to do, then calls other functions in this file to process data
+# - Outputs: None
+
+
+
+
+# Purpose: Fetch the data from the database (or google drive api)
+# 
+# Constraints:
+# - Inputs: All parameters for database access (user/auth info, what content to fetch)
+# - Outputs: The requested data
+
+
+
+
+# Purpose: Send the retrieved data to the frontend
+# 
+# Constraints:
+# - Inputs: The data to send
+# - Effects: Sends data to frontend
+# - Outputs: None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
